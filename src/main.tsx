42d6c0884d4cc337aa4b7cae9938fcb63f007849
wshier00-ctrl <wshier00@gmail.com>
2025-09-16 16:41:35 -0400

    fix: remove .tsx extension from import

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import App from './App.tsx';
+import App from "./App";
 import './index.css';
 
 const qc = new QueryClient();
